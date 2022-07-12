f25159414bd8ce942282d0697be82c7fdb0596c6
Jenay Sermon <jenay.sermon@gmail.com>
2022-07-12 11:06:58 -0400

    my first r script
    first r script, used a data set and made a histogram in blue during day 2 of the LASER, orientation modules.

---
+++ notes.R
@@ -0,0 +1,11 @@
+download packages - tidyverse 
+have to call it when writing
+
+functions - verbs mean()
+objects - nouns of R language mean(student_grades)
+arguments - adverbs or direct objects mean (student_grades, na.rm = FALSE)
+
+control/enter is help menu
+
+punctuation or operator. average_grade <- mean(student_grades, na.rm = FALSE) 
+the pipe operator is the way to passing along information into the next function. 
